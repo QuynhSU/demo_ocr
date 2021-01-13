e885d8953fbe1dc0f943f21385898d647ff7a26a
QuynhSU <Phamthiquynh21098@gmail.com>
2021-01-13 08:44:01 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,157 +1,163 @@
+from collections import namedtuple
+import warnings
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset
-from torch.autograd import Variable
-import os
-import cv2
-import numpy as np
-from torchvision import datasets, models, transforms
-from tqdm import tqdm
-from preprocessing import pre_process
-label_len = 30
-vocab =  "<,.+:-?$ 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ>"
-# start symbol <
-# end symbol >
-char2token = {"PAD":0}
-token2char = {0:"PAD"}
-for i, c in enumerate(vocab):
-    char2token[c] = i+1
-    token2char[i+1] = c
+from torch import nn, Tensor
+import torch.nn.functional as F
+from utils import load_state_dict_from_url
+from typing import Callable, Any, Optional, Tuple, List
+from layer import Mish
+
+__all__ = ['Inception3', 'inception_v3', 'InceptionOutputs', '_InceptionOutputs']
 
 
-def illegal(label):
-    if len(label) > label_len-1:
-        return True
-    for l in label:
-        if l not in vocab[1:-1]:
-            # print('check',l)
-            return True
-    return False
+model_urls = {
+    # Inception v3 ported from TensorFlow
+    'inception_v3_google': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
+}
+
+InceptionOutputs = namedtuple('InceptionOutputs', ['logits', 'aux_logits'])
+InceptionOutputs.__annotations__ = {'logits': Tensor, 'aux_logits': Optional[Tensor]}
+
+# Script annotations failed with _GoogleNetOutputs = namedtuple ...
+# _InceptionOutputs set here for backwards compat
+_InceptionOutputs = InceptionOutputs
 
 
-class ListDataset(Dataset):
-    def __init__(self, fname, training=False):
-        self.lines = []
-        # if not isinstance(fname, list):
-        #     fname = [fname]
-        # for f in fname:
-        lines = open(fname).readlines()
-        # for i in lines :
-        #     print(i.strip('\n').split(", ")[0])
-            # if not illegal(i.strip('\n').split(", ")[1].replace(" ", "").replace("\"", "")):
-            #     print(i)
-        self.lines += [i for i in lines if not illegal(i.strip('\n').split(", ")[1].replace(" ", "").replace("\"", "")) ]
-        self.training = training
-        # print(self.lines)
-    def __len__(self):
-        return len(self.lines)
-    
-    def __getitem__(self, index):
-        '''
-        line: image path\tlabel
-        '''
-        line = self.lines[index]
-        # print(line)
-        img_path, label_y_str = line.strip('\n').split(', ')
-        label_y_str= label_y_str.replace(" ", "").replace("\"", "")
-        if self.training:
-            img_path = os.path.join("IC15/train", img_path.replace("ï»¿",'').replace("\ufeff", ""))
-        else:
-            img_path = os.path.join("IC15/test", img_path.replace("ï»¿",'').replace("\ufeff", ""))
-        img = cv2.imread(img_path)
-        # print(img_path)
-        # print(type(img))
-        # if img is None:
-        #     return None
-        img = pre_process(img)
-        img = img / 255.
-        # Channels-first
-        # img= np.resize(img, (16, 48, 3))
+def inception_v3(pretrained = False):
+    r"""Inception v3 model architecture from
+    `"Rethinking the Inception Architecture for Computer Vision" <http://arxiv.org/abs/1512.00567>`_.
+    .. note::
+        **Important**: In contrast to the other models the inception_v3 expects tensors with a size of
+        N x 3 x 299 x 299, so ensure your images are sized accordingly.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+        aux_logits (bool): If True, add an auxiliary branch that can improve training.
+            Default: *True*
+        transform_input (bool): If True, preprocesses the input according to the method with which it
+            was trained on ImageNet. Default: *False*
+    """
+    model = Inception3(init_weights=not pretrained)
+    if pretrained:
+        pretrained_dict = torch.load('inception_v3_google-1a9a5a14.pth')
+        # model = Inception3(**kwargs)
+        # state_dict = load_state_dict_from_url(model_urls['inception_v3_google'])
+        model_dict = model.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and v.size() == model_dict[k].size()}
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+    # return Inception3(**kwargs)
+
+
+class Inception3(nn.Module):
+
+    def __init__(self, init_weights = False):
+        super(Inception3, self).__init__()
         
-        # print(img.shape)
-        img = np.transpose(img, (2, 0, 1))
-        # As pytorch tensor
-        img = torch.from_numpy(img).float()
-        label = np.zeros(label_len, dtype=int)
-        for i, c in enumerate('<'+label_y_str):
-            label[i] = char2token[c]
-        label = torch.from_numpy(label)
-
-        label_y = np.zeros(label_len, dtype=int)
-        for i, c in enumerate(label_y_str+'>'):
-            label_y[i] = char2token[c]
-        label_y = torch.from_numpy(label_y) 
+        inception_blocks = [
+            BasicConv2d, InceptionA
+        ]
+        if init_weights is None:
+            warnings.warn('The default weight initialization of inception_v3 will be changed in future releases of '
+                          'torchvision. If you wish to keep the old behavior (which leads to long initialization times'
+                          ' due to scipy/scipy#11299), please set init_weights=True.', FutureWarning)
+            init_weights = True
+        # assert len(inception_blocks) == 7
+        conv_block = inception_blocks[0]
+        inception_a = inception_blocks[1]
         
-        return img, label_y, label, label_y_str
+        self.Conv2d_1a_3x3 = conv_block(3, 32, kernel_size=3, stride=2)
+        self.Conv2d_2a_3x3 = conv_block(32, 32, kernel_size=3)
+        self.Conv2d_2b_3x3 = conv_block(32, 64, kernel_size=3, padding=1)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.Conv2d_3b_1x1 = conv_block(64, 80, kernel_size=1)
+        self.Conv2d_4a_3x3 = conv_block(80, 192, kernel_size=3)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.Mixed_5b = inception_a(192, pool_features=32)
+        self.Mixed_5c = inception_a(256, pool_features=64)
+        self.Mixed_5d = inception_a(288, pool_features=64)
 
-def subsequent_mask(size):
-    "Mask out subsequent positions."
-    attn_shape = (1, size, size)
-    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
-    return torch.from_numpy(subsequent_mask) == 0
 
-def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & Variable(
-            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
-        return tgt_mask
-
-class Batch:
-    "Object for holding a batch of data with mask during training."
-    def __init__(self, imgs, trg_y, trg, pad=0):
-        self.imgs = Variable(imgs.cuda().float(), requires_grad=False)
-        self.src_mask = Variable(torch.from_numpy(np.ones([imgs.size(0), 1, 377], dtype=np.bool)).cuda().float())
-        if trg is not None:
-            self.trg = Variable(trg.cuda().long(), requires_grad=False)
-            self.trg_y = Variable(trg_y.cuda().long(), requires_grad=False)
-            self.trg_mask = \
-                self.make_std_mask(self.trg, pad)
-            self.ntokens = (self.trg_y != pad).data.sum()
-
-    @staticmethod
-    def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & Variable(
-            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
-        return Variable(tgt_mask.cuda(), requires_grad=False)
-
-class FeatureExtractor(nn.Module):
-    def __init__(self, submodule, name):
-        super(FeatureExtractor, self).__init__()
-        self.submodule = submodule
-        self.name = name
     def forward(self, x):
-        for name, module in self.submodule._modules.items():
-            x = module(x)
-            if name is self.name:
-                b = x.size(0)
-                c = x.size(1)
-                return x.view(b, c, -1).permute(0, 2, 1)
-        return None
-
-# if __name__=='__main__':
-#     listdataset = ListDataset('your-lines')
-#     dataloader = torch.utils.data.DataLoader(listdataset, batch_size=2, shuffle=False, num_workers=0)
-#     for epoch in range(1):
-#         for batch_i, (imgs, labels_y, labels) in enumerate(dataloader):
-#             continue
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # N x 3 x 299 x 299
+        x = self.Conv2d_1a_3x3(x)
+        # N x 32 x 149 x 149
+        x = self.Conv2d_2a_3x3(x)
+        # N x 32 x 147 x 147
+        x = self.Conv2d_2b_3x3(x)
+        # N x 64 x 147 x 147
+        x = self.maxpool1(x)
+        # N x 64 x 73 x 73
+        x = self.Conv2d_3b_1x1(x)
+        # N x 80 x 73 x 73
+        x = self.Conv2d_4a_3x3(x)
+        # N x 192 x 71 x 71
+        x = self.maxpool2(x)
+        # N x 192 x 35 x 35
+        x = self.Mixed_5b(x)
+        # N x 256 x 35 x 35
+        x = self.Mixed_5c(x)
+        # N x 288 x 35 x 35
+        x = self.Mixed_5d(x)
+        # N x 288 x 35 x 35
+       
+     
+     
+        return x
 
 
 
 
+class InceptionA(nn.Module):
 
+    def __init__(self, in_channels,pool_features,conv_block = None) :
+        super(InceptionA, self).__init__()
+        if conv_block is None:
+            conv_block = BasicConv2d
+        self.branch1x1 = conv_block(in_channels, 64, kernel_size=1)
+
+        self.branch5x5_1 = conv_block(in_channels, 48, kernel_size=1)
+        self.branch5x5_2 = conv_block(48, 64, kernel_size=5, padding=2)
+
+        self.branch3x3dbl_1 = conv_block(in_channels, 64, kernel_size=1)
+        self.branch3x3dbl_2 = conv_block(64, 96, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = conv_block(96, 96, kernel_size=3, padding=1)
+
+        self.branch_pool = conv_block(in_channels, pool_features, kernel_size=1)
+
+    def _forward(self, x):
+        branch1x1 = self.branch1x1(x)
+
+        branch5x5 = self.branch5x5_1(x)
+        branch5x5 = self.branch5x5_2(branch5x5)
+
+        branch3x3dbl = self.branch3x3dbl_1(x)
+        branch3x3dbl = self.branch3x3dbl_2(branch3x3dbl)
+        branch3x3dbl = self.branch3x3dbl_3(branch3x3dbl)
+
+        branch_pool = F.avg_pool2d(x, kernel_size=3, stride=1, padding=1)
+        branch_pool = self.branch_pool(branch_pool)
+
+        outputs = [branch1x1, branch5x5, branch3x3dbl, branch_pool]
+        return outputs
+
+    def forward(self, x) :
+        outputs = self._forward(x)
+        return torch.cat(outputs, 1)
+
+
+
+
+class BasicConv2d(nn.Module):
+
+    def __init__(self, in_channels: int,out_channels: int, **kwargs) :
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+        self.act = Mish()
+    def forward(self, x) :
+        x = self.conv(x)
+        x = self.bn(x)
+        # return F.relu(x, inplace=True)
+        return self.act(x)
