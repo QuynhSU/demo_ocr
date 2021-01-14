3c7ece95f00053617bf6590710261a257e6c5991
QuynhSU <Phamthiquynh21098@gmail.com>
2021-01-14 17:23:58 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,88 +1,159 @@
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset
-from torch.autograd import Variable
-import os
-import cv2
+''' Datasets
+    This file contains definitions for our CIFAR, ImageFolder, and HDF5 datasets
+'''
 import numpy as np
-from torchvision import datasets, models, transforms
-from tqdm import tqdm
+# from synthesis_plate import gen_syn_plate
+
+import os
+import os.path
+import sys
+from PIL import Image
+import numpy as np
+from tqdm import tqdm, trange
+
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torch.utils.data as data
+from torch.utils.data import DataLoader
+import cv2
+from torch.autograd import Variable
+import torch
 from preprocessing import pre_process
-label_len = 30
-vocab =  "<,.+:-?$ 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ>"
+
+
+max_seq_len = 10
+vocab = "<!\"()*,-.0126789:?ABCDEGHKLMNOPQRSTVXabcdeghiklmnopqrstuvxyÁÍÔÝàáâãèéêìíòóôõùúýăĐđĩũƠơưạảẤấầẩẫậắằẳẵặẹẻẽếềểễệỉịọỏốồổỗộớờởỡợụủứừửữựỳỵỷỹ“…>"
 # start symbol <
 # end symbol >
-char2token = {"PAD":0}
-token2char = {0:"PAD"}
+char2token = {"PAD": 0}
+token2char = {0: "PAD"}
 for i, c in enumerate(vocab):
     char2token[c] = i+1
     token2char[i+1] = c
 
 
-def illegal(label):
-    if len(label) > label_len-1:
-        return True
-    for l in label:
-        if l not in vocab[1:-1]:
-            # print('check',l)
-            return True
-    return False
+def load_image(path):
+    im = cv2.imread(path)
+    #im_bw = cv2.resize(im, (256,64))
+    return im
 
 
-class ListDataset(Dataset):
-    def __init__(self, fname, training=False):
-        self.lines = []
-        # if not isinstance(fname, list):
-        #     fname = [fname]
-        # for f in fname:
-        lines = open(fname).readlines()
-        # for i in lines :
-        #     print(i.strip('\n').split(", ")[0])
-            # if not illegal(i.strip('\n').split(", ")[1].replace(" ", "").replace("\"", "")):
-            #     print(i)
-        self.lines += [i for i in lines if not illegal(i.strip('\n').split(", ")[1].replace(" ", "").replace("\"", "")) ]
+def read_label(anno_path):
+    f = open(anno_path, "rt")
+    label = f.read().strip().replace('\n', '')
+    f.close()
+    return label
+
+
+def load_by_folder_path(imgs_path, annos_path, type):
+    n_image = 0
+    X = []
+    y = []
+    # count = 0
+    for r, d, f in os.walk(imgs_path):
+        # print(f)
+        for file in f:
+            if type in file:
+                #print(str(n_image) + ". Load image " + file)
+                image_path = os.path.join(r, file)
+                # print(image_path)
+                anno_path = os.path.join(
+                    annos_path, file.replace(type, '.txt'))
+                label = read_label(anno_path)
+                label = label.replace('@', '')
+                # count +=1
+                # print(count)    
+                if '$' in label:
+                    label = ''
+                label = label.upper()
+
+                X.append(image_path)
+                y.append(label)
+                n_image += 1
+        
+    return X, y
+
+
+class Dataset(data.Dataset):
+    """A generic data loader where the images are arranged in this way: ::
+        root/dogball/xxx.png
+        root/dogball/xxy.png
+        root/dogball/xxz.png
+        root/cat/123.png
+        root/cat/nsdf3.png
+        root/cat/asd932_.png
+    Args:
+        root (string): Root directory path.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+     Attributes:
+        classes (list): List of the class names.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __init__(self, data_path, dataset_name, training):
+        
+        annos_path = os.path.join(data_path, '{}/annos'.format(dataset_name))
+        imgs_path = os.path.join(data_path, '{}/imgs'.format(dataset_name))
+#         print("okay")
+        self.imgs, self.labels = load_by_folder_path(imgs_path, annos_path, '.png')
+#         print("check . . . . ")
+        #x_val, y_val = data_load_path(val_path, '.jpg')
+        # print("check . . . . ")
+        print("Number images: ", len(self.imgs))
         self.training = training
-        # print(self.lines)
-    def __len__(self):
-        return len(self.lines)
-    
+        self.transform = transforms.Compose([transforms.ToTensor()]) 
     def __getitem__(self, index):
-        '''
-        line: image path\tlabel
-        '''
-        line = self.lines[index]
-        # print(line)
-        img_path, label_y_str = line.strip('\n').split(', ')
-        label_y_str= label_y_str.replace(" ", "").replace("\"", "")
-        if self.training:
-            img_path = os.path.join("IC15/train", img_path.replace("ï»¿",'').replace("\ufeff", ""))
-        else:
-            img_path = os.path.join("IC15/test", img_path.replace("ï»¿",'').replace("\ufeff", ""))
-        img = cv2.imread(img_path)
-        # print(img_path)
-        # print(type(img))
-        # if img is None:
-        #     return None
+        
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        path, label = self.imgs[index], self.labels[index]
+        img = cv2.imread(path)
         img = pre_process(img)
-        img = img / 255.
-        # Channels-first
-        # img= np.resize(img, (16, 48, 3))
+        original_img = np.copy(img)
         
-        # print(img.shape)
-        img = np.transpose(img, (2, 0, 1))
-        # As pytorch tensor
-        img = torch.from_numpy(img).float()
-        label = np.zeros(label_len, dtype=int)
-        for i, c in enumerate('<'+label_y_str):
-            label[i] = char2token[c]
-        label = torch.from_numpy(label)
+        # if self.training:
+        #     img, label = gen_syn_plate(img, label)
 
-        label_y = np.zeros(label_len, dtype=int)
-        for i, c in enumerate(label_y_str+'>'):
-            label_y[i] = char2token[c]
-        label_y = torch.from_numpy(label_y) 
+        img = img/255.0
+        img = np.moveaxis(img, 2, 0)
+
+        img = torch.from_numpy(img).float()
+        label_token = np.zeros(max_seq_len, dtype=int)
+        #print(path, label, len(label))
+        for i, c in enumerate('<'+label):
+            try:
+                label_token[i] = char2token[c]
+            except:
+#                 print(path)
+                break
+        label_token = torch.from_numpy(label_token)
+        label_token_y = np.zeros(max_seq_len, dtype=int)
+        for i, c in enumerate(label + '>'):
+            try:
+                label_token_y[i] = char2token[c]
+            except:
+#                 print(path)
+                break
+        label_token_y = torch.from_numpy(label_token_y)
+        # original_img = self.transform(self.imgs[index])
+        # label_token = self.transform(self.labels[index])
+        return img,  label_token_y, label_token, label
+
         
-        return img, label_y, label, label_y_str
+        
+
+    def __len__(self):
+        return len(self.imgs)
+
 
 def subsequent_mask(size):
     "Mask out subsequent positions."
@@ -90,21 +161,25 @@ def subsequent_mask(size):
     subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
     return torch.from_numpy(subsequent_mask) == 0
 
+
 def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & Variable(
-            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
-        return tgt_mask
+    "Create a mask to hide padding and future words."
+    tgt_mask = (tgt != pad).unsqueeze(-2)
+    tgt_mask = tgt_mask & Variable(
+        subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+    return tgt_mask
+
 
 class Batch:
     "Object for holding a batch of data with mask during training."
+
     def __init__(self, imgs, trg_y, trg, pad=0):
-        self.imgs = Variable(imgs.cuda().float(), requires_grad=False)
-        self.src_mask = Variable(torch.from_numpy(np.ones([imgs.size(0), 1, 377], dtype=np.bool)).cuda().float())
+        self.imgs = Variable(imgs, requires_grad=False)
+        self.src_mask = Variable(torch.from_numpy(
+            np.ones([imgs.size(0), 1, 377], dtype=np.bool)).cuda())
         if trg is not None:
-            self.trg = Variable(trg.cuda().long(), requires_grad=False)
-            self.trg_y = Variable(trg_y.cuda().long(), requires_grad=False)
+            self.trg = Variable(trg.cuda(), requires_grad=False)
+            self.trg_y = Variable(trg_y.cuda(), requires_grad=False)
             self.trg_mask = \
                 self.make_std_mask(self.trg, pad)
             self.ntokens = (self.trg_y != pad).data.sum()
@@ -116,42 +191,3 @@ class Batch:
         tgt_mask = tgt_mask & Variable(
             subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
         return Variable(tgt_mask.cuda(), requires_grad=False)
-
-class FeatureExtractor(nn.Module):
-    def __init__(self, submodule, name):
-        super(FeatureExtractor, self).__init__()
-        self.submodule = submodule
-        self.name = name
-    def forward(self, x):
-        for name, module in self.submodule._modules.items():
-            x = module(x)
-            if name is self.name:
-                b = x.size(0)
-                c = x.size(1)
-                return x.view(b, c, -1).permute(0, 2, 1)
-        return None
-
-# if __name__=='__main__':
-#     listdataset = ListDataset('your-lines')
-#     dataloader = torch.utils.data.DataLoader(listdataset, batch_size=2, shuffle=False, num_workers=0)
-#     for epoch in range(1):
-#         for batch_i, (imgs, labels_y, labels) in enumerate(dataloader):
-#             continue
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
